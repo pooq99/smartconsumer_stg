0a78edb8dec512281187b7ae3067f1e09219f032
jk.shin <pooq99@gmail.com>
2019-06-23 12:51:50 +0900

    201906231251

--- precache-manifest.1a586fc4e4f646850e37ec991ef3a49e.js
+++ precache-manifest.1a586fc4e4f646850e37ec991ef3a49e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "223cc551eacc1a070f3da40bef721f2f",
+    "revision": "e8270b10a3d007336c4c5e157853de84",
     "url": "https://stg.smartconsumer.kr/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://stg.smartconsumer.kr/static/css/2.3c1ddb0a.chunk.css"
   },
   {
-    "revision": "285f827e55bca1a08005",
+    "revision": "44300e56e1fbb91e832d",
     "url": "https://stg.smartconsumer.kr/static/css/main.ef304509.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "https://stg.smartconsumer.kr/static/js/2.1205d0f0.chunk.js"
   },
   {
-    "revision": "285f827e55bca1a08005",
-    "url": "https://stg.smartconsumer.kr/static/js/main.6a32fade.chunk.js"
+    "revision": "44300e56e1fbb91e832d",
+    "url": "https://stg.smartconsumer.kr/static/js/main.f2af172c.chunk.js"
   },
   {
     "revision": "2921ab725ba4465a8e1c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://stg.smartconsumer.kr/precache-manifest.eb4870d9a71e398c2a43a1af5639fbc0.js"
+  "https://stg.smartconsumer.kr/precache-manifest.1a586fc4e4f646850e37ec991ef3a49e.js"
 );
 
 self.addEventListener('message', (event) => {
