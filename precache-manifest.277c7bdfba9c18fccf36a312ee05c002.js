ffbbaae38ea32b1e20a138e687c620ae10643fc8
jk.shin <pooq99@gmail.com>
2019-07-25 17:13:22 +0900

    201907251713

---
+++ precache-manifest.277c7bdfba9c18fccf36a312ee05c002.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9f465b306266cee88ea7ec80187f9b",
+    "url": "https://stg.smartconsumer.kr/index.html"
+  },
+  {
+    "revision": "a787322350e60823bd3f",
+    "url": "https://stg.smartconsumer.kr/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "14c62620ff25dae59063",
+    "url": "https://stg.smartconsumer.kr/static/js/2.d206361d.chunk.js"
+  },
+  {
+    "revision": "a787322350e60823bd3f",
+    "url": "https://stg.smartconsumer.kr/static/js/main.a7d3e2a2.chunk.js"
+  },
+  {
+    "revision": "49c63daf01db552efb51",
+    "url": "https://stg.smartconsumer.kr/static/js/runtime~main.75877a31.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.931a2496aacf58be6d7548e2ce22dc04.js"
+  "https://stg.smartconsumer.kr/precache-manifest.277c7bdfba9c18fccf36a312ee05c002.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://stg.smartconsumer.kr/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
