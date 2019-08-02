745c494dc64232cc25183a87998016899d0cefc2
jk.shin <pooq99@gmail.com>
2019-08-02 15:25:29 +0900

    201908021525

---
+++ precache-manifest.489a4c8f2d39866345036bf3267f0507.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad52eb0e7558116f98e98933cf29c77",
+    "url": "https://stg.smartconsumer.kr/index.html"
+  },
+  {
+    "revision": "e35bf7107f82e5bc7df6",
+    "url": "https://stg.smartconsumer.kr/static/css/main.9e526bb0.chunk.css"
+  },
+  {
+    "revision": "981ca1cdafc07e369d11",
+    "url": "https://stg.smartconsumer.kr/static/js/2.92fcaceb.chunk.js"
+  },
+  {
+    "revision": "e35bf7107f82e5bc7df6",
+    "url": "https://stg.smartconsumer.kr/static/js/main.a0dfd1e9.chunk.js"
+  },
+  {
+    "revision": "49c63daf01db552efb51",
+    "url": "https://stg.smartconsumer.kr/static/js/runtime~main.75877a31.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://stg.smartconsumer.kr/precache-manifest.489a4c8f2d39866345036bf3267f0507.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://stg.smartconsumer.kr/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
